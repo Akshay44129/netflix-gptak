806f47d6266b38eaa2d40a034392a21049896ffe
Akshay44129 <akshaysakariya123@gmail.com>
2025-03-25 22:51:35 +0530

    update api key in firebase for sequrity purpose

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,13 +2,16 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDAfsehDx-omTZqt0AuL477M0GbNyru5e0",
+  apiKey:  process.env.REACT_APP_GOOGLE_API_KEY,
   authDomain: "netflix-gptak.firebaseapp.com",
   projectId: "netflix-gptak",
   storageBucket: "netflix-gptak.firebasestorage.app",
